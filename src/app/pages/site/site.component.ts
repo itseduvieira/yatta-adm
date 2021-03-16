50ba1d13e22892aa83df4ba415c7e9f4a11cdc47
Eduardo Vieira <eduardofelipevieira@gmail.com>
2021-03-15 22:22:22 -0300

    wip

--- src/app/pages/site/site.component.ts
+++ src/app/pages/site/site.component.ts
@@ -9,204 +9,219 @@ import { of, throwError } from 'rxjs';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-site',
-  templateUrl: './site.component.html',
-  styleUrls: ['./site.component.scss']
+    selector: 'app-site',
+    templateUrl: './site.component.html',
+    styleUrls: ['./site.component.scss']
 })
 export class SiteComponent implements OnInit, AfterViewInit {
 
-  stripe: any;
-  data: any;
-  card: any;
+    stripe: any;
+    data: any;
+    card: any;
 
-  isChecked: boolean;
-  isComplete: boolean;
-  isLoading: boolean;
-  requestComplete: boolean;
-  
-  title = 'Your payment transaction succeeded, you are good to go';
-  subtitle = 'Take full advantage of yatta! right away';
-  imgFeedback = '/assets/img/success.png';
+    isChecked: boolean;
+    isComplete: boolean;
+    isLoading: boolean;
+    requestComplete: boolean;
 
-  nextActionUrl: string;
+    title = 'Your payment transaction succeeded, you are good to go';
+    subtitle = 'Take full advantage of yatta! right away';
+    imgFeedback = '/assets/img/success.png';
 
-  @ViewChild('pricing', { static: false }) pricing: ElementRef;
+    nextActionUrl: string;
 
-  constructor(
-    private modalService: NgbModal,
-    private authService: AuthenticationService, 
-    private paymentService: PaymentService,
-    private router: Router
-  ) {
-    
-  }
-  ngAfterViewInit(): void {
-    const url = this.router.url;
-    if(url.indexOf('/payment') > -1) {
-      this.pricing.nativeElement.click();
+    @ViewChild('pricing', { static: false }) pricing: ElementRef;
+
+    constructor(
+        private modalService: NgbModal,
+        private authService: AuthenticationService,
+        private paymentService: PaymentService,
+        private router: Router
+    ) {
+
     }
-  }
+    ngAfterViewInit(): void {
+        const url = this.router.url;
+        if (url.indexOf('/payment') > -1) {
+            this.pricing.nativeElement.click();
+        }
+    }
 
-  ngOnInit() {
-    
-  }
+    ngOnInit() {
 
-  tryAgain() {
-    this.isChecked = true;
-    this.isLoading = false;
-    this.isComplete = false;
-    this.requestComplete = false;
-    this.card.clear();
-  }
+    }
 
-  openModal(content) {
-    of(loadStripe(environment.stripeKey, { locale: 'en' }))
-      .pipe(first())
-      .subscribe(async result => {
-        this.stripe = await result;
-
-        this.modalService.open(content, { size: 'lg', backdrop: 'static' });
-
+    tryAgain() {
         this.isChecked = true;
         this.isLoading = false;
         this.isComplete = false;
         this.requestComplete = false;
-
-        var elements = this.stripe.elements();
-        this.card = elements.create('card', {
-          hidePostalCode: true,
-          style: {
-            base: {
-              color: "#525f7f",
-              fontFamily: 'Open Sans, sans-serif',
-              fontSmoothing: "antialiased",
-              fontSize: "1em",
-              "::placeholder": {
-                color: "#525f7f"
-              }
-            },
-            invalid: {
-              fontFamily: 'Open Sans, sans-serif',
-              color: "#fa755a",
-              iconColor: "#fa755a"
-            }
-          } 
-        });
-
-        this.card.mount('#card-info');
-        this.card.on('change', event => {
-          this.isComplete = event.complete;
-        });
-      });
-  }
-
-  async checkTwitter() {
-    const user = await this.authService.loginWithTwitter();
-
-    if(user.profile.subscription.status === 'active') {
-      this.router.navigate(['/dash']);
-    } else {
-      this.router.navigate(['/payment']);
-    }
-  }
-
-  async subscribe() {
-    this.isLoading = true;
-
-    let currentUser = await this.authService.loginWithTwitter();
-    
-    if(currentUser.profile.subscription && 
-      currentUser.profile.subscription.status === 'active') {
-        this.requestComplete = true;
-        this.title = 'You already have an active subscription'
-        this.subtitle = 'Don\'t worry, your card wasn\'t charged at all. Just take advantage of yatta! features';
-        this.imgFeedback = '/assets/img/success.png';
-
-        return Promise.resolve();
+        this.card.clear();
     }
 
-    await this.createSubscription();
-  }
+    openModal(content) {
+        of(loadStripe(environment.stripeKey, { locale: 'en' }))
+            .pipe(first())
+            .subscribe(async result => {
+                this.stripe = await result;
 
-  goToDash() {
-    this.router.navigate(['/dash'])
-      .then(() => {
-        this.modalService.dismissAll();
-      });
-  }
+                this.modalService.open(content, { size: 'lg', backdrop: 'static' });
 
-  async confirmPayment() {  
-    if(this.nextActionUrl) {
-      window.location.href = this.nextActionUrl;
+                this.isChecked = true;
+                this.isLoading = false;
+                this.isComplete = false;
+                this.requestComplete = false;
 
-      this.nextActionUrl = null;
-    } else {
-      this.router.navigate(['/dash']);
+                var elements = this.stripe.elements();
+                this.card = elements.create('card', {
+                    hidePostalCode: true,
+                    style: {
+                        base: {
+                            color: "#525f7f",
+                            fontFamily: 'Open Sans, sans-serif',
+                            fontSmoothing: "antialiased",
+                            fontSize: "1em",
+                            "::placeholder": {
+                                color: "#525f7f"
+                            }
+                        },
+                        invalid: {
+                            fontFamily: 'Open Sans, sans-serif',
+                            color: "#fa755a",
+                            iconColor: "#fa755a"
+                        }
+                    }
+                });
+
+                this.card.mount('#card-info');
+                this.card.on('change', event => {
+                    this.isComplete = event.complete;
+                });
+            });
     }
-  }
 
-  async createSubscription() {
-    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    async checkTwitter() {
+        const user = await this.authService.loginWithTwitter();
 
-    const result = await this.stripe.createPaymentMethod({
-      type: 'card',
-      card: this.card,
-      billing_details: {
-        name: currentUser.profile.screen_name,
-      },
-    });
+        if (user.profile.subscription.status === 'active') {
+            this.router.navigate(['/dash']);
+        } else {
+            this.router.navigate(['/payment']);
+        }
+    }
 
-    if (result.error) {
-      this.requestComplete = true;
-      this.title = 'We have an issue processing your payment'
-      this.subtitle = 'Please check if your card is valid, if it has enough funds or if it can be charged in US$';
-      this.imgFeedback = '/assets/img/fail.png';
+    async subscribe() {
+        this.isLoading = true;
 
-    } else {
-      this.paymentService.createSubscription({
-        customerId: currentUser.profile.subscription.customerId,
-        paymentMethodId: result.paymentMethod.id,
-        priceId: this.isChecked ? environment.priceAnnual : environment.priceMonthly,
-      })
-        .pipe(first())
-        .pipe(catchError(error => {
-          return throwError(error);
-        }))
-        .subscribe(async result => {
+        let currentUser = await this.authService.loginWithTwitter();
 
-          const lastPaymentIntent = result.latest_invoice.payment_intent;
-
-          if(lastPaymentIntent.status === 'requires_action') {
-            
-            const confirm = await this.stripe.confirmPaymentIntent(
-              lastPaymentIntent.client_secret,
-              {
-                payment_method: lastPaymentIntent.payment_method,
-                return_url: `${window.location.origin}/#/dash`
-              }
-            );
-
-            this.title = 'Action required: confirm the payment';
-            this.subtitle = 'Hit the button below to confirm the payment, then log in again to check you subscription status';
-            this.imgFeedback = '/assets/img/confirm.png';
-
-            this.nextActionUrl = confirm.paymentIntent.next_action.redirect_to_url.url;
-
+        if (currentUser.profile.subscription &&
+            currentUser.profile.subscription.status === 'active') {
             this.requestComplete = true;
-          } else {
-            this.title = 'Hooray! Your payment has succeeded, you are good to go';
-            this.subtitle = 'Enjoy your subscription and take full advantage of yatta! right now';
+            this.title = 'You already have an active subscription'
+            this.subtitle = 'Don\'t worry, your card wasn\'t charged at all. Just take advantage of yatta! features';
             this.imgFeedback = '/assets/img/success.png';
 
-            this.requestComplete = true;
-          }
-        }, result => {
-          this.title = `We have an issue processing your payment: ${result.error.error.message}`;
-          this.subtitle = 'Please check if your card is valid, if it has enough funds and it can be charged in US$';
-          this.imgFeedback = '/assets/img/fail.png';
+            return Promise.resolve();
+        }
 
-          this.requestComplete = true;
-        })
+        await this.createSubscription();
     }
-  }
+
+    goToDash() {
+        this.router.navigate(['/dash'])
+            .then(() => {
+                this.modalService.dismissAll();
+            });
+    }
+
+    async confirmPayment() {
+        if (this.nextActionUrl) {
+            window.location.href = this.nextActionUrl;
+
+            this.nextActionUrl = null;
+        } else {
+            this.router.navigate(['/dash']);
+        }
+    }
+
+    async createSubscription() {
+        const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
+        const result = await this.stripe.createPaymentMethod({
+            type: 'card',
+            card: this.card,
+            billing_details: {
+                name: currentUser.profile.screen_name,
+            },
+        });
+
+        if (result.error) {
+            this.requestComplete = true;
+            this.title = 'We have an issue processing your payment'
+            this.subtitle = 'Please check if your card is valid, if it has enough funds or if it can be charged in US$';
+            this.imgFeedback = '/assets/img/fail.png';
+
+        } else {
+            this.paymentService.createSubscription({
+                customerId: currentUser.profile.subscription.customerId,
+                paymentMethodId: result.paymentMethod.id,
+                priceId: this.isChecked ? environment.priceAnnual : environment.priceMonthly,
+            })
+                .pipe(first())
+                .pipe(catchError(error => {
+                    return throwError(error);
+                }))
+                .subscribe(async result => {
+
+                    const lastPaymentIntent = result.latest_invoice.payment_intent;
+
+                    if (lastPaymentIntent && lastPaymentIntent.status === 'requires_action') {
+
+                        const confirm = await this.stripe.confirmPaymentIntent(
+                            lastPaymentIntent.client_secret,
+                            {
+                                payment_method: lastPaymentIntent.payment_method,
+                                return_url: `${window.location.origin}/#/dash`
+                            }
+                        );
+
+                        this.title = 'Action required: confirm the payment';
+                        this.subtitle = 'Hit the button below to confirm the payment, then log in again to check you subscription status';
+                        this.imgFeedback = '/assets/img/confirm.png';
+
+                        this.nextActionUrl = confirm.paymentIntent.next_action.redirect_to_url.url;
+
+                        this.requestComplete = true;
+                    } else if (lastPaymentIntent && lastPaymentIntent.status === 'requires_payment_method' &&
+                        lastPaymentIntent.last_payment_error) {
+                        this.title = `We have an issue processing your payment: ${lastPaymentIntent.last_payment_error.message}`;
+                        this.subtitle = 'Please check if your card is valid, if it has enough funds and if it can be charged in US$';
+                        this.imgFeedback = '/assets/img/fail.png';
+
+                        this.requestComplete = true;
+                    } else {
+                        this.title = 'Hooray! Your payment has succeeded, you are good to go';
+                        this.subtitle = 'Enjoy your subscription and take full advantage of yatta! right now';
+                        this.imgFeedback = '/assets/img/success.png';
+
+                        this.requestComplete = true;
+                    }
+                }, result => {
+                    let message
+
+                    if(result.error.code && result.error.code === 'card_declined') {
+                        message = result.error.raw.message
+                    } else {
+                        message = result.error.error.message
+                    }
+
+                    this.title = `We have an issue processing your payment: ${message}`;
+                    this.subtitle = 'Please check if your card is valid, if it has enough funds and if it can be charged in US$';
+                    this.imgFeedback = '/assets/img/fail.png';
+
+                    this.requestComplete = true;
+                })
+        }
+    }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,11 @@ export const environment = {
     appId: "1:917008487741:web:4ef6f2227615e967f96154",
     measurementId: "G-RN8RFEF5MY"
   },
-  stripeKey: 'pk_test_0mG0iApRlgQsZiYoV6x36Jq100yXbImTIw',
+//   stripeKey: 'pk_test_0mG0iApRlgQsZiYoV6x36Jq100yXbImTIw',
+    stripeKey: 'pk_live_vXjweAeRCTjMahplBxihoLuu004KOeZgM3',
   priceMonthly: 'price_1IT7UEC4t66flnypdqhr535Q',
   priceAnnual: 'price_1IT7V5C4t66flnyp48ILSnSp'
+    
+
+// priceAnnual: 'price_1IVQ93C4t66flnypEZnFhvyg'
 };
