ece1a213c2b757600d28fa51af40d1225a7b82fc
Eduardo Vieira <eduardofelipevieira@gmail.com>
2021-03-15 22:23:31 -0300

    wip

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,20 +1,16 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3001',
-  firebaseConfig: {
-    apiKey: "AIzaSyCAU667SEK2Q4AMitvdjJLLG78y6VXKilM",
-    authDomain: "personal-4ace7.firebaseapp.com",
-    projectId: "personal-4ace7",
-    storageBucket: "personal-4ace7.appspot.com",
-    messagingSenderId: "917008487741",
-    appId: "1:917008487741:web:4ef6f2227615e967f96154",
-    measurementId: "G-RN8RFEF5MY"
-  },
-//   stripeKey: 'pk_test_0mG0iApRlgQsZiYoV6x36Jq100yXbImTIw',
-    stripeKey: 'pk_live_vXjweAeRCTjMahplBxihoLuu004KOeZgM3',
-  priceMonthly: 'price_1IT7UEC4t66flnypdqhr535Q',
-  priceAnnual: 'price_1IT7V5C4t66flnyp48ILSnSp'
-    
-
-// priceAnnual: 'price_1IVQ93C4t66flnypEZnFhvyg'
+    production: false,
+    apiUrl: 'http://localhost:3001',
+    firebaseConfig: {
+        apiKey: "AIzaSyCAU667SEK2Q4AMitvdjJLLG78y6VXKilM",
+        authDomain: "personal-4ace7.firebaseapp.com",
+        projectId: "personal-4ace7",
+        storageBucket: "personal-4ace7.appspot.com",
+        messagingSenderId: "917008487741",
+        appId: "1:917008487741:web:4ef6f2227615e967f96154",
+        measurementId: "G-RN8RFEF5MY"
+    },
+    stripeKey: 'pk_test_0mG0iApRlgQsZiYoV6x36Jq100yXbImTIw',
+    priceMonthly: 'price_1IT7UEC4t66flnypdqhr535Q',
+    priceAnnual: 'price_1IT7V5C4t66flnyp48ILSnSp'
 };
