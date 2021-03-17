1355f37e4f5d5c86f065d87a9431e8aff6829f1f
Eduardo Vieira <eduardofelipevieira@gmail.com>
2021-03-17 19:31:16 -0300

    wip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,22 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://yattanalytics.herokuapp.com',
-  firebaseConfig: {
-    apiKey: "AIzaSyCAU667SEK2Q4AMitvdjJLLG78y6VXKilM",
-    authDomain: "personal-4ace7.firebaseapp.com",
-    projectId: "personal-4ace7",
-    storageBucket: "personal-4ace7.appspot.com",
-    messagingSenderId: "917008487741",
-    appId: "1:917008487741:web:4ef6f2227615e967f96154",
-    measurementId: "G-RN8RFEF5MY"
-  },
-  stripeKey: 'pk_live_vXjweAeRCTjMahplBxihoLuu004KOeZgM3',
-  priceMonthly: 'price_1IT7UOC4t66flnypyBMUoJP1',
-  priceAnnual: 'price_1IT7VAC4t66flnypTF5ZBTgS'
+    production: true,
+    apiUrl: 'https://yattanalytics.herokuapp.com',
+    firebaseConfig: {
+        apiKey: "AIzaSyCAU667SEK2Q4AMitvdjJLLG78y6VXKilM",
+        authDomain: "personal-4ace7.firebaseapp.com",
+        projectId: "personal-4ace7",
+        storageBucket: "personal-4ace7.appspot.com",
+        messagingSenderId: "917008487741",
+        appId: "1:917008487741:web:4ef6f2227615e967f96154",
+        measurementId: "G-RN8RFEF5MY"
+    },
+    stripeKey: 'pk_live_vXjweAeRCTjMahplBxihoLuu004KOeZgM3',
+    priceMonthly: {
+        usd: 'price_1IT7UOC4t66flnypyBMUoJP1', // 3
+        brl: 'price_1IW7ptC4t66flnyp7xh1yBfD'  // 7.5
+    },
+    priceAnnual: {
+        usd: 'price_1IT7VAC4t66flnypTF5ZBTgS', // 24
+        brl: 'price_1IW7pRC4t66flnyp2oiScRo5'  // 60
+    }
 };
